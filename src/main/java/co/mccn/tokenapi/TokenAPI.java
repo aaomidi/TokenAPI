44c089320a84572ab9506c15fd054fe7038aed39
Brandon Currell <branicyeti@live.co.uk>
2013-12-19 10:25:43 +0000

    My version. Take a look; do we use it?

--- src/main/java/co/mccn/tokenapi/TokenAPI.java
+++ src/main/java/co/mccn/tokenapi/TokenAPI.java
@@ -1,23 +1,151 @@
 package co.mccn.tokenapi;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.File;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-public class TokenAPI extends JavaPlugin {
-    public final Logger logger = Logger.getLogger("Minecraft");
-    protected HashMap<String, Integer> playerBalance = new HashMap<>();
-    protected DatabaseHandler mysql;
+public class TokenAPI extends JavaPlugin
+{
+
+    private Database database;
+    private Map<String, Integer> tokenMap = new LinkedHashMap<>();
 
     @Override
-    public void onEnable() {
-        saveDefaultConfig();
-        mysql = new DatabaseHandler(this);
-        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
-        logger.log(Level.INFO, "{0} v{1} has been enabled.", new Object[]{getDescription().getName(), getDescription().getVersion()});
-        this.getCommand("tokenapi").setExecutor(new Commands(this));
+    public final void onDisable()
+    {
+        if (this.database != null)
+        {
+            this.database.disconnect();
+        }
+    }
 
+    @Override
+    public final void onLoad()
+    {
+        if (!new File(this.getDataFolder(), "config.yml").exists())
+        {
+            this.saveDefaultConfig();
+            this.getLogger().severe("THIS IS NOT A PLUGIN. THIS IS AN API FOR OTHER PLUGINS TO USE. DO NOT EXPECT THIS PLUGIN TO DO ANYTHING.");
+        }
+    }
+
+    @Override
+    public final void onEnable()
+    {
+        try
+        {
+            this.database = new Database(this);
+        }
+        catch (SQLException ex)
+        {
+            this.getLogger().severe("Error connecting to database! Disabling...");
+            ex.printStackTrace();
+            this.getPluginLoader().disablePlugin(this);
+            return;
+        }
+
+        this.updateTokenMap();
+    }
+
+    public final Map<String, Integer> getTokenMap()
+    {
+        return this.tokenMap;
+    }
+
+    public final int getTokens(Player player)
+    {
+        return this.getTokens(player.getName());
+    }
+
+    public final int getTokens(String playerName)
+    {
+        if (!this.tokenMap.containsKey(playerName))
+        {
+            this.updateTokenMap(playerName);
+        }
+
+        return this.tokenMap.get(playerName);
+    }
+
+    public final void setTokens(Player player, int amount)
+    {
+        this.setTokens(player.getName(), amount);
+    }
+
+    public final void setTokens(String playerName, int amount)
+    {
+        this.tokenMap.put(playerName, amount);
+    }
+
+    public final void updateTokens(Player player, int diff)
+    {
+        this.updateTokens(player.getName(), diff);
+    }
+
+    public final void updateTokens(String playerName, int diff)
+    {
+        this.setTokens(playerName, this.getTokens(playerName) + diff);
+    }
+
+    public final void updateTokenMap()
+    {
+        String query = "SELECT `player`, `tokens` FROM `tokens`";
+        ResultSet resultSet = this.database.executeQuery(query);
+
+        try
+        {
+            if (resultSet == null || resultSet.isClosed())
+            {
+                return;
+            }
+
+            while (resultSet.next())
+            {
+                this.tokenMap.put(resultSet.getString("player"), resultSet.getInt("tokens"));
+            }
+        }
+        catch (SQLException ex)
+        {
+            /*
+             * Before anyone (Zenexer) loses their shit, this is debug and will be removed once I can confirm
+             * I am doing MySQL correctly.
+             */
+            ex.printStackTrace();
+        }
+    }
+
+    public final void updateTokenMap(String playerName)
+    {
+        String query = "SELECT `tokens` FROM `tokens` WHERE `player`=?";
+        ResultSet resultSet = this.database.executeQuery(query, playerName);
+
+        try
+        {
+            if (resultSet != null)
+            {
+                this.tokenMap.put(resultSet.getString("player"), resultSet.getInt("tokens"));
+            }
+        }
+        catch (SQLException ex)
+        {
+            /*
+             * Before anyone (Zenexer) loses their shit, this is debug and will be removed once I can confirm
+             * I am doing MySQL correctly.
+             */
+            ex.printStackTrace();
+        }
+    }
+
+    public final void updateDatabase()
+    {
+    }
+
+    public final void updateDatabase(String playerName)
+    {
     }
 }
